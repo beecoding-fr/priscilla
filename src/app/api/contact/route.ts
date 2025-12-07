fab7a2434bcd3500dc858c95126e34c0d41f0bd0
Thomas Deruche <deruche32@hotmail.com>
2025-12-07 11:51:30 +0100

    fix: fixes CI build

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -2,10 +2,9 @@ import { NextResponse } from "next/server";
 import { contactSchema } from "@/lib/validations";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(request: Request) {
   try {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     const body = await request.json();
     const validatedData = contactSchema.parse(body);
 
